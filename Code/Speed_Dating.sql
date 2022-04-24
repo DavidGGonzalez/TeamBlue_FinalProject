1da7666f5140c0fdff4fb929f169096aa2d6b2df
ana.gajardoni@gmail.com <ana.gajardoni@gmail.com>
2022-04-24 11:01:06 -0400

    S3 - AG - Addition of field origin and all its implications

--- Code/Speed_Dating.sql
+++ Code/Speed_Dating.sql
@@ -20,6 +20,9 @@ CREATE TABLE "person" (
     "importance_race" int   NOT NULL,
     "importance_religion" int   NOT NULL,
     "goal" int   NOT NULL,
+    "origin" char(100)   ,
+    "origin2" char(100)   ,
+	
     "freq_date" int   NOT NULL,
     "freq_go_out" int   NOT NULL,
     "interest_sports" int   NOT NULL,
